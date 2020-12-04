b2148a892f0fa5db73ae608a7b0ec682c0c9a2bc
kateherz <68287182+kateherz@users.noreply.github.com>
2020-12-04 10:59:51 -0500

    Add files via upload
    working timer

--- KatesFreezeria.java
+++ KatesFreezeria.java
@@ -2,12 +2,12 @@
 import javax.swing.JFrame;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
-
+import java.util.concurrent.TimeUnit;
 public class KatesFreezeria					   
 {
    public static Freezeria screen;					//Our Custom JPanel
 
-   public static void main(String[]args)
+   public static void main(String[]args) throws InterruptedException
    {
       screen = new Freezeria();
       JFrame frame = new JFrame("Kate's Freezeria");	//window title
@@ -17,7 +17,27 @@ public class KatesFreezeria
       frame.setContentPane(screen);		
       frame.setVisible(true);
       	      //  MouseListeners would go in the JPanel (screen)
-   }
+      boolean x=true; //https://stackoverflow.com/questions/10820033/make-a-simple-timer-in-java/14323134
+            long displayMinutes=0;
+            long starttime=System.currentTimeMillis();
+            System.out.println("Timer:");
+            while(x)
+            {
+               TimeUnit.SECONDS.sleep(1);
+               long timepassed=System.currentTimeMillis()-starttime;
+               long secondspassed=timepassed/1000;
+               if(secondspassed==60)
+               {
+                  secondspassed=0;
+                  starttime=System.currentTimeMillis();
+               }
+               if((secondspassed%60)==0)
+                  displayMinutes++;
+            
+               System.out.println(displayMinutes+"::"+secondspassed);
+            }
+         }
+      
   
  
 }
