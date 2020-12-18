0119404081a082e506d6d25b7b684d3d7bece105
kateherz <68287182+kateherz@users.noreply.github.com>
2020-12-18 10:41:52 -0500

    Add files via upload
    added flavor buttons, randomized flavor selection

--- Freezeria.java
+++ Freezeria.java
@@ -39,6 +39,7 @@ public class Freezeria extends JPanel implements MouseListener, MouseMotionListe
     //***BUTTON CODE***define array of buttons
    private Button [] buttons = new Button[4]; 
    private Button [] continueButton = new Button[1];               //buttons to activate features
+   private Button [] flavorButtons = new Button[3];
    //*****************/
    
    protected static int mouseX;			      //locations for the mouse pointer
@@ -50,12 +51,20 @@ public class Freezeria extends JPanel implements MouseListener, MouseMotionListe
    private Player[] customers= new Player[1];
    private ImageIcon[] utah = {new ImageIcon("Utah.png")};
    
+   private int properOrderFlavor; //integer for the proper flavor customer wants
+   private String OrderTicketFlavor;//the actual name of the flavor customer wants
+   private int playerOrderFlavor;//the integer for the flavor that gameplayer clicked
+   private String[] flavorWords= { "Peanut Butter Cup", "Strawberry","Blueberry"};//all possible flavors
+   
    private static final int STARTSCREEN=0, ORDER_STATION=1, BUILD=2, MIX = 3, TOPPING=4, EVALUATION=5; //different game modes
    private static int gameMode;                                       
    public Freezeria()
    {
       gameMode = STARTSCREEN;               
       customers[0]= new Player("utah", SIZEx-200, SIZEy-370, 0, utah, 10, SIZEy);
+      
+      properOrderFlavor=(int)(Math.random()*(2))+1;
+      OrderTicketFlavor= flavorWords[properOrderFlavor];
      
       frame=0;
       
@@ -69,16 +78,26 @@ public class Freezeria extends JPanel implements MouseListener, MouseMotionListe
       Shape r3 = new Ellipse2D.Float(600, SIZEy-100,100, 50);
       Shape r4 = new Ellipse2D.Float(750, SIZEy-100,100, 50);
       Shape r5 = new Ellipse2D.Float(900, SIZEy-100,100, 50);
-      //Shape r6 = new Rect(900, SIZEy-100,100, 50);
-      //ImageIcon buttonImage1 = new ImageIcon("graphics/button1.jpg");
-      //ImageIcon buttonImage2 = new ImageIcon("graphics/button2.jpg");
+      Shape r6 = new Rectangle(205, 163,140, 115);//int x, int y, width height
+      Shape r7 = new Rectangle(51, 163,140, 115);//int x, int y, width height
+      Shape r8 = new Rectangle(897, 163,140, 115);//int x, int y, width height
+      
+      
       buttons[0] = new Button(r2, "Order",  new Color(100,222,80), Color.YELLOW, Color.BLACK);
       buttons[1] = new Button(r3, "Build", new Color(125,73,213), Color.YELLOW, Color.BLACK);
       buttons[2] = new Button(r4, "Mix", new Color(232,157,45), Color.YELLOW, Color.BLACK);
       buttons[3] = new Button(r5, "Toppings", new Color(54,76,220), Color.YELLOW, Color.BLACK);
-      //buttons[4] = new Button(r5, "Toppings", Color.CYAN, Color.YELLOW, Color.BLACK);
       continueButton[0] = new Button(r2, "CONTINUE", new Color(233,54,90), Color.YELLOW, Color.BLACK);
       //buttons[2] = new Button(r3, "sound", buttonImage1, buttonImage2);
+   
+      ImageIcon flavorButtonImage1 = new ImageIcon("PeanutButterCup.png");
+      ImageIcon flavorButtonImage2 = new ImageIcon("Strawberry.png");
+      ImageIcon flavorButtonImage3 = new ImageIcon("Blueberry.png");
+      flavorButtons[0] = new Button(r6, "", flavorButtonImage1, flavorButtonImage2);
+      flavorButtons[1] = new Button(r7, "", flavorButtonImage2, flavorButtonImage1);
+      flavorButtons[2] = new Button(r8, "", flavorButtonImage3, flavorButtonImage1);
+      
+   
    
    }
 
@@ -102,6 +121,7 @@ public class Freezeria extends JPanel implements MouseListener, MouseMotionListe
          ImageIcon buildImage = new ImageIcon("build.png");
          g.drawImage(buildImage.getImage(), 0,0,SIZEx,SIZEy, null);
          drawButtons(buttons, g);
+         drawButtons(flavorButtons, g);
       }
       else if(gameMode == MIX)
       {
@@ -164,7 +184,20 @@ public class Freezeria extends JPanel implements MouseListener, MouseMotionListe
                else if(b.getTitle().equals("Toppings"))
                   gameMode=4;
             }
-         }   
+         } 
+         for(Button b: flavorButtons) 
+         {
+            if(b.getShape().contains(mouseX, mouseY))
+            {
+               if(b.getImageIcon().equals("PeanutButterCup.png"))
+                  playerOrderFlavor = 1;
+               else if(b.getImageIcon().equals("Strawberry.png"))
+                  playerOrderFlavor = 2;
+               else if(b.getImageIcon().equals("Blueberry.png"))
+                  playerOrderFlavor = 3;
+            }
+         
+         } 
       } 
       else if(button == MouseEvent.BUTTON3)//right click
       {
@@ -255,6 +288,11 @@ public class Freezeria extends JPanel implements MouseListener, MouseMotionListe
             if(customers[0].getX()>200 && frame%100==0)
             {
                customers[0].setX(customers[0].getX()-1);
+            }
+            if(customers[0].getX()==200)
+            {
+               Shape order = new Ellipse2D.Float(450, SIZEy-500,100, 50);//how do I get a shape to just pop up
+            //order.drawShape(g);
             }
             
             
